3b847453769c7a0bc21f48cc591ffbe9e02058bb
arustan1 <60210961+arustan1@users.noreply.github.com>
2020-04-12 15:14:28 -0500

    Created recipes and ingredients classes

--- src/main/java/dmacc/beans/Recipes.java
+++ src/main/java/dmacc/beans/Recipes.java
@@ -3,4 +3,58 @@ package dmacc.beans;
 public class Recipes {
 	private long id;
 	private String name;
+	private String category;
+	
+	
+	public Recipes() {
+		super();
+	}
+	
+
+	public Recipes(long id, String name, String category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
 }
