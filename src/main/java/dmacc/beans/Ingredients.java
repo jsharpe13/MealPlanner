011a7bc7a891e47dfcd1a63c4464d2c9c2b43eee
arustan1 <60210961+arustan1@users.noreply.github.com>
2020-04-12 15:06:58 -0500

    Initial commit

--- src/main/java/dmacc/beans/Ingredients.java
+++ src/main/java/dmacc/beans/Ingredients.java
@@ -1,5 +1,7 @@
 package dmacc.beans;
 
 public class Ingredients {
-
+	private long id;
+	private String name;
+	private String category;
 }
--- src/main/java/dmacc/beans/Recipes.java
+++ src/main/java/dmacc/beans/Recipes.java
@@ -1,5 +1,6 @@
 package dmacc.beans;
 
 public class Recipes {
-
+	private long id;
+	private String name;
 }
